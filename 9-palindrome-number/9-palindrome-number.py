f399cb7e0c154cf758c1f27741b91e3d625bd1c6
Prabhakar Singh <pksingh15007@gmail.com>
2022-05-23 13:07:37 +0530

    Time: 71 ms (62.82%), Space: 13.4 MB (64.97%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -4,15 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x<0:
-            return False
+        rev = 0
+        temp = x
+        while (temp>0):
+            rev = rev*10 + temp%10
+            temp = temp//10
+        if rev == x:
+            return True
         else:
-            rev = 0
-            temp = x
-            while (temp>0):
-                rev = rev*10 + temp%10
-                temp = temp//10
-            if rev == x:
-                return True
-            else:
-                return False
+            return False
