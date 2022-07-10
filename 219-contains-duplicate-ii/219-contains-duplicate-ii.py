b710aab5437570e9e2d62e7ee4bc0c3202d13004
Prabhakar Singh <pksingh15007@gmail.com>
2022-07-10 17:13:31 +0530

    Time: 873 ms (34.60%), Space: 24.8 MB (39.07%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -6,7 +6,9 @@ class Solution(object):
         :rtype: bool
         """
         hash_map = {}
-        for i in range(len(nums)):
+        first = 0
+        last = len(nums)
+        for i in range(first,last):
             num = nums[i]
             if num in hash_map and abs(i-hash_map[num])<=k:
                 return True
